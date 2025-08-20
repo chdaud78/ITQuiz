45116cc5a827e57b3ba6ef431a3c712eb461dd82
박총명 <qkrri56@naver.com>
2025-08-20 15:13:41 +0900

    [ENH] 마이페이지, notfound 구성
    route 작동 확인
    마이페이지, notfound 화면 기초 구성

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import RootLayout from '@/layouts/RootLayout.jsx'
 import { ROUTES } from '@/lib/routes.js'
 import Home from '@/routes/Home.jsx'
+import Mypage from '@/routes/Mypage.jsx'
+import NotFound from '@/routes/NotFound.jsx'
 
 const AppRouters = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
+        <Route path={ROUTES.MYPAGE.ROOT} element={<Mypage />} />
       </Route>
+      <Route path={ROUTES.NOT_FOUND} element={<NotFound />}></Route>
     </Routes>
   </BrowserRouter>
 )
